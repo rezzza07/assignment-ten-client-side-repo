b5fdbebda3a6d671a9d6212140d001a07005b8c3
rezzza07 <notfunnyatall0007@gmail.com>
2025-11-10 13:42:20 +0600

    Setup

---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Home from './components/Home/Home.jsx';
+import RootLayout from './layout/RootLayout.jsx';
+import ExploreArtworks from './components/ExploreArtworks/ExploreArtworks.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayout,
+    children:[
+      {
+        index:true,
+        Component: Home
+      },
+      {
+        path: 'exploreArtworks',
+        Component: ExploreArtworks
+      }
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+)
